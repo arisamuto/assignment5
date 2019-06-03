15fd915549d66fb0ac92aba6f16851ef7ba839b7
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-04 08:42:35 +0900

    almost

--- assignment5/ViewController.swift
+++ assignment5/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var fruitlabel: UILabel!
+    @IBOutlet var vegelabel: UILabel!
+    @IBOutlet var fruitTextFeild: UITextField!
+    @IBOutlet var vegeTextFeild: UITextField!
+    @IBOutlet var savebutton: UIButton!
+    
+    
+    var wordArray: [Dictionary<String, String>] = []
+    let saveData = UserDefaults.standard
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if saveData.array(forKey: "WORD") != nil {
+            wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String, String>]
+        }
+   
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    @IBAction func saveWord() {
+        let vocab = ["kudamono": fruitTextFeild.text!, "yasai": vegeTextFeild.text!]
+        wordArray.append(vocab)
+        saveData.set(wordArray, forKey: "WORD")
+        
+    }
+    
 }
 
