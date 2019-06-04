2e717ab64877db86857f8277319b2c97e86b313f
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-05 05:27:23 +0900

    ..

--- assignment5/ViewController.swift
+++ assignment5/ViewController.swift
@@ -10,34 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var fruitlabel: UILabel!
-    @IBOutlet var vegelabel: UILabel!
     @IBOutlet var fruitTextFeild: UITextField!
     @IBOutlet var vegeTextFeild: UITextField!
     @IBOutlet var savebutton: UIButton!
     
-    
-    var wordArray: [Dictionary<String, String>] = []
+    var dictionary: [String:String] = [:]
+    var tosave: [String:String] = [:]
     let saveData = UserDefaults.standard
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if saveData.array(forKey: "veges") != nil {
-            wordArray = saveData.array(forKey: "veges") as! [Dictionary<String, String>]
-        }
-   
+     
+        
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    @IBAction func saveWord() {
-        let vocab = ["kudamono": fruitTextFeild.text!, "yasai": vegeTextFeild.text!]
-        wordArray.append(vocab)
-        saveData.set(wordArray, forKey: "veges")
-        
+    //保存ボタン
+    @IBAction func saveWord(_sender:Any) {
+        tosave = ["fruits": fruitTextFeild.text!, "veges": vegeTextFeild.text!]
+        saveData.set(tosave, forKey: "WORD")
+        saveData.set(fruitTextFeild.text, forKey: "fruits")
+        saveData.set(vegeTextFeild.text, forKey: "veges")
+
     }
     
+    
 }
-
