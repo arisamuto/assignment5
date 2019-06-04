d175ef9592fffe67cf606f4094232bc77d1cdc0f
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-05 05:48:33 +0900

    DONE

--- assignment5/ViewController.swift
+++ assignment5/ViewController.swift
@@ -14,27 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet var vegeTextFeild: UITextField!
     @IBOutlet var savebutton: UIButton!
     
-    var dictionary: [String:String] = [:]
+    var toloaddictionary: [String:String] = [:]
     var tosave: [String:String] = [:]
     let saveData = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-     
-        
+        //データを確認
+        if saveData.dictionary(forKey: "WORD") != nil {
+            toloaddictionary = saveData.object(forKey: "WORD")as! [String:String]
+            print(toloaddictionary)
+            fruitTextFeild.text = toloaddictionary ["fruits"]
+            vegeTextFeild.text = toloaddictionary ["veges"]
+            
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
     //保存ボタン
     @IBAction func saveWord(_sender:Any) {
         tosave = ["fruits": fruitTextFeild.text!, "veges": vegeTextFeild.text!]
         saveData.set(tosave, forKey: "WORD")
         saveData.set(fruitTextFeild.text, forKey: "fruits")
         saveData.set(vegeTextFeild.text, forKey: "veges")
-
     }
-    
-    
 }
